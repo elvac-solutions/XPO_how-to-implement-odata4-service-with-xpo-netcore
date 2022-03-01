c626e6f0b048fa720a036c86b3df305dbe3acd5b
ELVAC SOLUTIONS <svatopluk.ulicny.dx@gmail.com>
2022-03-01 13:46:33 +0100

    ValueConverter on OData selected property is not executed
    Signed-off-by: ELVAC SOLUTIONS <svatopluk.ulicny.dx@gmail.com>

--- CS/ODataService/Models/Contract.cs
+++ CS/ODataService/Models/Contract.cs
@@ -1,14 +1,26 @@
 using DevExpress.Xpo;
+using System;
+using ODataService.ValueConverters;
 
-namespace ODataService.Models {
+namespace ODataService.Models
+{
     [Persistent("Contracts")]
-    public class Contract : BaseDocument {
+    public class Contract : BaseDocument
+    {
 
         public Contract(Session session) : base(session) { }
         public Contract() { }
 
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+
+            DateInsert = DateTime.Now;
+        }
+
         string fNumber;
-        public string Number {
+        public string Number
+        {
             get { return fNumber; }
             set { SetPropertyValue<string>(nameof(Number), ref fNumber, value); }
         }
@@ -17,9 +29,29 @@ namespace ODataService.Models {
         [Size(5)]
         [Association(@"ContractsReferencesCustomers")]
         [Persistent("CustomerID")]
-        public Customer Customer {
+        public Customer Customer
+        {
             get { return fCustomerID; }
             set { SetPropertyValue<Customer>(nameof(Customer), ref fCustomerID, value); }
         }
+
+        #region DateInsert (DateTime)
+        /// <summary>
+        /// TODO: Doplnit komentář k vlastnosti <see cref="DateInsert" />
+        /// </summary>
+        [ValueConverter(typeof(GeneralDateTimeConverter))]
+        public DateTime DateInsert
+        {
+            get => dateInsert;
+            set => SetPropertyValue(StartDatePropertyName, ref dateInsert, value);
+        }
+
+        private DateTime dateInsert;
+
+        public const string StartDatePropertyName = nameof(DateInsert);
+        #endregion DateInsert (DateTime)
+
+
+
     }
 }
---
+++ CS/ODataService/ValueConverters/GeneralDateTimeConverter.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.Xpo.Metadata;
+using System;
+
+namespace ODataService.ValueConverters
+{
+    public class GeneralDateTimeConverter : ValueConverter
+    {
+        public override object ConvertFromStorageType(object value)
+        {
+            object result = null;
+            if (value != null)
+            {
+                result = DateTime.SpecifyKind((DateTime)value, DateTimeKind.Utc).ToLocalTime();
+            }
+            return result;
+        }
+
+        public override object ConvertToStorageType(object value)
+        {
+            object result = null;
+            if (value != null)
+            {
+                DateTime date = (DateTime)value;
+                if (date.Kind == DateTimeKind.Unspecified)
+                {
+                    result = DateTime.SpecifyKind(date, DateTimeKind.Local).ToUniversalTime();
+                }
+                else if (date.Kind == DateTimeKind.Local)
+                {
+                    result = (object)date.ToUniversalTime();
+                }
+                else
+                {
+                    result = (object)date;
+                }
+            }
+            return result;
+        }
+
+        public override Type StorageType { get { return typeof(DateTime); } }
+    }
+
+}
